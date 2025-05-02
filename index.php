ee425287a31fd81f41e4cb06d0930caa466d0d06
Rodrigo807 <rodrigo.oliveira87@ua.pt>
2025-05-02 13:12:47 +0100

    Menu admin adicionado a um ficheiro separado
    Quando servidor da estga voltar ao ar, trocarei os links para cerem compatíveis

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 session_start();
 ?>
 
@@ -16,8 +16,8 @@ session_start();
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css" rel="stylesheet">
   <script src="src/js/script.js"></script>
   <?php
-    $base_url = "/~ptaw-2025-gr4";
-?>
+  $base_url = "/~ptaw-2025-gr4";
+  ?>
 
 
   <?php include 'src/includes/carrinho.php'; ?>
--- src/Admin/Dashboard.php
+++ src/Admin/Dashboard.php
@@ -1,4 +1,5 @@
 <?php
+
 session_start();
 
 if (!isset($_SESSION['admin_email'])) {
@@ -17,112 +18,249 @@ if (!isset($_SESSION['admin_email'])) {
 <title>Print & Go</title>
 <link rel="stylesheet" href="css/Dashboard.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
-    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-<script src="js/Dashboard.js"></script>
+  integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<script src="js/Dashboard.js">
+
+</script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-    crossorigin="anonymous"></script>
+  integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+  </script>
+
+
+<style>
+  body {
+    margin: 0;
+    padding: 0;
+    overflow: auto;
+    align-items: center;
+  }
+
+  #menu-mobile {
+    display: none;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 80%;
+    height: 100%;
+    background-color: white;
+    transform: translateX(-10%);
+    transition: transform 2000ms ease;
+    overflow: hidden;
+    z-index: 999;
+    flex-direction: column-reverse;
+    margin-top: 0;
+    justify-content: flex-end;
+    gap: 20px;
+    align-items: center;
+    padding-top: 100px;
+  }
+
+  .fixed-header {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    background-color: white;
+    z-index: 999;
+    display: flex;
+    align-items: center;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  }
+
+  #menu-mobile.open {
+    display: flex;
+    transform: translateX(0);
+  }
+
+  #menu-mobile a {
+    color: #4F46E5;
+    text-decoration: none;
+    font-size: 1.5rem;
+    margin: 15px 0;
+  }
+
+  #menu-toggle {
+    display: none;
+    position: fixed;
+    top: 15px;
+    left: 15px;
+    z-index: 1000;
+    font-size: 2rem;
+    background: none;
+    border: none;
+    color: black;
+    cursor: pointer;
+  }
+
+  #logo-header-mobile {
+    display: none;
+    background: none;
+    border: none;
+  }
+
+
+  /* Ajustes para telas menores */
+  @media (max-width: 1200px) {
+    body {
+      width: 100%;
+      padding-top: 70px;
+    }
+
+    .header-desktop {
+      display: none !important;
+    }
+
+    #a-logo-header-mobile {
+      display: flex;
+      width: max-content;
+      justify-content: center;
+      padding-top: 20px;
+      padding-bottom: 20px;
+      width: 100%;
+
+    }
+
+    #logo-header-mobile {
+      display: block;
+      width: 100px;
+      height: auto;
+      margin: 0 auto;
+    }
+
+    #menu-toggle {
+      display: block;
+      top: 0;
+    }
+
+    #menu-mobile {
+      position: fixed;
+      height: 100%;
+      overflow: hidden;
+    }
+
+    #menu-mobile li {
+      list-style: none;
+    }
+
+    #menu-mobile .social {
+      display: flex;
+      height: 100%;
+      width: 100%;
+      align-items: center;
+      align-self: flex-end;
+      justify-content: center;
+    }
+
+    .container1 {
+      display: none;
+    }
+
+    .left,
+    .right {
+      width: 100%;
+      justify-content: center;
+    }
+
+    .left a {
+      width: auto;
+    }
+
+    #containerHeroe {
+      flex-direction: column;
+      height: auto;
+      padding: 20px 20px;
+      width: 100%;
+    }
+
+    .esqHeroe,
+    .dirHeroe {
+      width: 100%;
+      text-align: center;
+    }
+
+    .dirHeroe img {
+      position: static;
+      height: auto;
+      width: 80%;
+    }
+
+    #prodDestaques {
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+
+    }
+
+    .containerDestaques {
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      width: 100%;
+      gap: 20px;
+    }
+
+    .feedback-carousel-container {
+      max-width: 90%;
+      max-height: 400px;
+    }
+
+    #featureSection {
+      padding: 5vh;
+    }
+
+    #containerFeatures {
+      flex-direction: column;
+    }
+
+    .featureBox p {
+      width: 90%;
+    }
+
+    #cta h3 {
+      text-align: center;
+      font-size: 100%;
+    }
+
+    #cta p {
+      text-align: center;
+      font-size: 90%;
+    }
+
+    #containerFooter {
+      flex-direction: column;
+      align-items: center;
+      gap: 20px;
+    }
+  }
+</style>
+
+
 </head>
 
 <body style="background-color: #E5E7EB;">
-    <div class="d-flex">
-        <!-- Menu lateral -->
-        <div class="d-flex flex-column flex-shrink-0 p-3 min-vh-100" id="div-menu" style="width: 280px;">
-            <!-- Logo -->
-            <a href="Dashboard.php"
-                class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-                <svg class="bi pe-none me-2" width="40" height="32">
-                    <use xlink:href="#bootstrap"></use>
-                </svg>
-                <span class="fs-3 fw-bold text-custom-color">Print&Go</span>
-            </a>
-            <hr>
-            <!-- Tabela com links -->
-            <ul class="nav  flex-column mb-auto">
-                <!-- Link ativo -->
-                <li class="nav-item nav-link.active">
-                    <a href="#" class="nav-link active" aria-current="page">
-                        <svg xmlns="http://www.w3.org/2000/svg"
-                            style="stroke:currentColor; stroke-width:1; color: #4F46E5;" width="16" height="16"
-                            fill="currentColor" class="bi pe-none me-2 bi-graph-up" viewBox="0 0 16 16">
-                            <path fill-rule="evenodd"
-                                d="M0 0h1v15h15v1H0zm14.817 3.113a.5.5 0 0 1 .07.704l-4.5 5.5a.5.5 0 0 1-.74.037L7.06 6.767l-3.656 5.027a.5.5 0 0 1-.808-.588l4-5.5a.5.5 0 0 1 .758-.06l2.609 2.61 4.15-5.073a.5.5 0 0 1 .704-.07" />
-                        </svg>
-                        Dashboard
-                    </a>
-                </li>
-                <!-- Links para a pagina de produtos do admin-->
-                <li>
-                    <a href="Products.html" class="nav-link link-body-emphasis">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                            class="bi me-2 bi-box2-fill" viewBox="0 0 16 16">
-                            <path
-                                d="M3.75 0a1 1 0 0 0-.8.4L.1 4.2a.5.5 0 0 0-.1.3V15a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1V4.5a.5.5 0 0 0-.1-.3L13.05.4a1 1 0 0 0-.8-.4zM15 4.667V5H1v-.333L1.5 4h6V1h1v3h6z" />
-                        </svg>
-                        Products
-                    </a>
-                </li>
-                <!-- Links para a pagina de pedidos do admin-->
-                <li>
-                    <a href="#" class="nav-link link-body-emphasis">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                            class="bi me-2 bi-cart-fill" viewBox="0 0 16 16">
-                            <path
-                                d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
-                        </svg>
-                        Orders
-                    </a>
-                </li>
-                <!-- Links para a pagina de analises do admin-->
-                <li>
-                    <a href="#" class="nav-link link-body-emphasis">
-                        <svg xmlns="http://www.w3.org/2000/svg" style="stroke:currentColor; stroke-width:1;" width="16"
-                            height="16" fill="currentColor" class="bi pe-none me-2 bi-graph-up" viewBox="0 0 16 16">
-                            <path fill-rule="evenodd"
-                                d="M0 0h1v15h15v1H0zm14.817 3.113a.5.5 0 0 1 .07.704l-4.5 5.5a.5.5 0 0 1-.74.037L7.06 6.767l-3.656 5.027a.5.5 0 0 1-.808-.588l4-5.5a.5.5 0 0 1 .758-.06l2.609 2.61 4.15-5.073a.5.5 0 0 1 .704-.07" />
-                        </svg>
-                        Analytics
-                    </a>
-                </li>
-                <!-- Links para a pagina de descontos do admin-->
-                <li>
-                    <a href="#" class="nav-link link-body-emphasis">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                            class="bi me-2 bi-tag-fill" viewBox="0 0 16 16">
-                            <path
-                                d="M2 1a1 1 0 0 0-1 1v4.586a1 1 0 0 0 .293.707l7 7a1 1 0 0 0 1.414 0l4.586-4.586a1 1 0 0 0 0-1.414l-7-7A1 1 0 0 0 6.586 1zm4 3.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0" />
-                        </svg>
-                        Discounts
-                    </a>
-                </li>
-                <!-- Links para a pagina de configurações do admin-->
-                <li>
-                    <a href="#" class="nav-link link-body-emphasis">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                            class="bi me-2 bi-gear-fill" viewBox="0 0 16 16">
-                            <path
-                                d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z" />
-                        </svg>
-                        Settings
-                    </a>
-                </li>
-            </ul>
-        </div>
+  <div class="d-flex">
 
-        <!-- Conteudo principal -->
-        <div class="flex-grow-1 p-4" id="main-content">
-
-          <?php echo ($_SESSION['admin_email']); ?>
-
-          <a href="/PTAW/src/logout.php" class="nav-link">
-            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5985E1">
-              <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h280v80H200Zm440-160-55-58 102-102H360v-80h327L585-622l55-58 200 200-200 200Z"/>
-            </svg>
-          </a>
+    <!-- Menu lateral -->
+    <?php include '../includes/header-desktop-admin.php'; ?>
+    <!-- Menu mobile -->
+    <?php include '../includes/header-mobile-admin.php'; ?>
 
 
-        </div>
+    <!-- Conteudo principal -->
+    <div class="flex-grow-1 p-4" id="main-content">
+
+      <?php echo ($_SESSION['admin_email']); ?>
+
+      <a href="/PTAW/src/logout.php" class="nav-link">
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5985E1">
+          <path
+            d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h280v80H200Zm440-160-55-58 102-102H360v-80h327L585-622l55-58 200 200-200 200Z" />
+        </svg>
+      </a>
+
+
     </div>
+  </div>
 
 
 
