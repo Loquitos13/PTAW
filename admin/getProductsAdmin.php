074e77efe13fd6771d07d938da4c4b9a9ed762f2
unknown <brunolopesjardim@ua.pt>
2025-06-26 13:52:29 +0100

    endpoint productsAdmin 26/06 13:52

---
+++ admin/getProductsAdmin.php
@@ -0,0 +1,59 @@
+<?php
+
+
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: GET, POST");
+
+require_once '../restapi/Database.php';
+
+$apiUrl = "http://estga-dev.ua.pt/~ptaw-2025-gr4/restapi/PrintGoAPI.php";
+
+function executeCurlRequest($ch) {
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($ch);
+    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+    if (curl_errno($ch)) {
+        $error = curl_error($ch);
+        curl_close($ch);
+        throw new Exception("CURL Error: $error");
+    }
+
+    curl_close($ch);
+
+    json_decode($response);
+    if(json_last_error() !== JSON_ERROR_NONE) {
+        throw new Exception("Invalid JSON response: " . json_last_error_msg());
+    }
+
+    return $response;
+}
+
+header('Content-Type: application/json');
+
+try {
+    $response = getProductsAdminData();
+    echo json_encode($response);
+} catch (Exception $e) {
+    http_response_code(400);
+    echo json_encode([
+        'error' => 'Request failed',
+        'message' => $e->getMessage()
+    ]);
+}
+
+function getProductsAdminData() {
+    global $apiUrl;
+
+    $ch = curl_init("$apiUrl/productsAdmin");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPGET, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        'Content-Type: application/json'
+    ]);
+
+    $response = executeCurlRequest($ch);
+
+    return json_decode($response, true);
+}
+?>
